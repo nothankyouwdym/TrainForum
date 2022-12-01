91095d32d621d853b4f9db4182a8f6f15967be96
nothankyouwdym <105501075+nothankyouwdym@users.noreply.github.com>
2022-12-01 14:02:05 -0500

    1 file modified

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -8,7 +8,9 @@ class Rules extends Component{
         <h1 style={{textAlign: "center"}}>Rules</h1>
 
         <ol>
-          <li></li>
+          <li>All disscussion here is about trains and busses specifacally which trains are delayed and inforation about arrivas.</li>
+          <li>If you talk about anything else you will not have access to the information.</li>
+          <li>Do not lie about information, because if we find out you will not have access to this information..</li>
         </ol>
       </section>
     )
