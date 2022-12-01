06ea7c6a27ef9d60c46b23dfb758b033dc5d3b85
nothankyouwdym <105501075+nothankyouwdym@users.noreply.github.com>
2022-12-01 10:08:52 -0500

    2 files modified, 1 added, 2 deleted

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+
+class Navbar extends Component{
+  render(){
+    return(
+      <nav>
+        
+
+        <span>hiii</span>
+        <span>fdfdf</span>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
