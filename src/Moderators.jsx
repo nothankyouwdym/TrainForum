8b13d254c0082ec6b936baddbe6b33b2ca6726ed
nothankyouwdym <105501075+nothankyouwdym@users.noreply.github.com>
2022-12-01 13:34:33 -0500

    1 file added

---
+++ src/Moderators.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+
+class Mods extends Component{
+  render(){
+    return(
+      <section>
+        <h1 style={{textAlign: "center"}}></h1>
+
+        <table>
+          
+        </table>
+      </section>
+    )
+  }
+}
+
+export default Mods
