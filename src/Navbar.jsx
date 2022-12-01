7c95a09e33c3a01ce8c3de4b31d066fb8d5dbfe9
nothankyouwdym <105501075+nothankyouwdym@users.noreply.github.com>
2022-12-01 10:12:17 -0500

    4 files modified

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react"
+import {Link} from "react-router-dom"
 
 
 class Navbar extends Component{
   render(){
     return(
       <nav>
-        
+        <span id="NavTitle">Train Forum</span>
 
-        <span>hiii</span>
-        <span>fdfdf</span>
+        <span>Home</span>
+        <span>About</span>
       </nav>
     )
   }
